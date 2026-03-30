7b60b496e76bd9ae0b48c5219248a0b9b8a0698e
Ravvah <achourrabah36@gmail.com>
2026-03-30 15:23:10 +0200

    change pakcage name and add dataloader mapping

---
+++ src/neural_cv/data/databuilder.py
@@ -0,0 +1,66 @@
+from typing import Dict
+import torch
+from torch import Tensor
+from torch.utils.data import DataLoader, Subset
+
+from torchvision.datasets import ImageFolder
+from neural_cv.data.processor import Processor
+
+
+class DataBuilder:
+
+    def __init__(self, data_dir: str, batch_size: int = 32, train_size: float = 0.8, val_size: float = 0.1, test_size: float = 0.1, seed: int = 42):
+        if not(0 < train_size < 1):
+            raise ValueError("train_size must be between 0 and 1")
+
+        if abs(train_size + val_size + test_size - 1.0) > 1e-6:
+            raise ValueError("train + val + test must sum to 1")
+        
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.tran_size = train_size
+        self.val_size = val_size
+        self.test_size = test_size
+        self.seed = seed
+
+    
+    def build_dataset(self) -> Dict[str, Dict[str, DataLoader]]:
+        torch.manual_seed(self.seed)
+
+        base_dataset = ImageFolder(self.data_dir)
+
+        indices = torch.randperm(len(base_dataset))
+        n = len(indices)
+
+        n_train = int(self.train_size * n)
+        n_val = int((1 - self.train_size - 0.1) * n)
+
+        train_idx = indices[: n_train]
+        val_idx = indices[n_train: n_train + n_val]
+        test_idx = indices[n_train + n_val:]
+
+        texture_dataset = ImageFolder(self.data_dir, transform=Processor(mode="texture"))
+        global_dataset = ImageFolder(self.data_dir, transform=Processor(mode="global"))
+
+        dataset_map = {
+            "texture": self._create_loaders(texture_dataset, train_idx, val_idx, test_idx),
+            "global": self._create_loaders(global_dataset, train_idx, val_idx, test_idx)
+        }
+
+        return dataset_map
+
+    
+    def _create_loaders(self, dataset: ImageFolder, train_idx: Tensor, val_idx: Tensor, test_idx: Tensor) -> Dict[str, DataLoader]:
+        train_dataset = Subset(dataset=dataset, indices=train_idx)
+        val_dataset = Subset(dataset=dataset, indices=val_idx)
+        test_dataset = Subset(dataset=dataset, indices=test_idx)
+
+        data_loaders_map = {
+            "train": DataLoader(dataset=train_dataset, batch_size=self.batch_size, shuffle=True),
+            "validation": DataLoader(dataset=val_dataset, batch_size=self.batch_size),
+            "test": DataLoader(dataset=test_dataset, batch_size=self.batch_size)
+        }
+        return data_loaders_map
+
+
+
---
+++ src/neural_cv/model/cnn.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.models.resnet import resnet152, resnet18
+from torchvision.models.convnext import convnext_large
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+
+import pytorch_lightning as pl
+
+
+
+class CNN(nn.Module):
+
+    def __init__(self, num_classes: int = 4):
+        super().__init__()
+
+        self.model = resnet152(progress=True, pretrained=True)
+        self.model.fc = nn.Linear(in_features=self.model.fc.in_features, out_features=num_classes)
+
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+
