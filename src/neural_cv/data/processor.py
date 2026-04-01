3cec302978faffa02eacdb814181e9627da85309
Ravvah <achourrabah36@gmail.com>
2026-04-01 11:47:49 +0200

    add visualization method of transformations

--- src/neural_cv/data/processor.py
+++ src/neural_cv/data/processor.py
@@ -23,12 +23,13 @@ class Processor:
         self.use_patch_shuffle = use_patch_shuffle
         self.use_high_pass_filter = use_high_pass_filter
         self.use_random_erasing = use_random_erasing
+        self.transforms_list = []
 
         if mode == "texture":
-            transforms_list = [
+            self.transforms_list = [
                 transforms.Resize(256),
-                transforms.RandomResizedCrop(224, scale=(0.5, 1.0)),
-                transforms.RandomGrayscale(p=0.5),
+                transforms.CenterCrop(224),
+                transforms.Grayscale(num_output_channels=3),
                 transforms.ColorJitter(
                     brightness=0.3,
                     contrast=0.3,
@@ -39,25 +40,21 @@ class Processor:
             ]
 
         elif mode == "global":
-            transforms_list = [
+            self.transforms_list = [
                 transforms.Resize(256),
-                transforms.RandomResizedCrop(
-                    size=224,
-                    scale=(0.80, 1.0),
-                    ratio=(0.95, 1.05)
-                ),
+                transforms.CenterCrop(size=224),
                 transforms.Resize(112, interpolation=transforms.InterpolationMode.BILINEAR),
                 transforms.Resize(224, interpolation=transforms.InterpolationMode.BILINEAR),
                 transforms.GaussianBlur(
-                    kernel_size=7,
-                    sigma=(0.8, 1.5)
+                    kernel_size=11,
+                    sigma=(2.0, 3.0)
                 ),
-                transforms.ColorJitter(
-                    brightness=0.08,
-                    contrast=0.05,
-                    saturation=0.05,
-                    hue=0.02
-                ),
+                # transforms.ColorJitter(
+                #     brightness=0.08,
+                #     contrast=0.05,
+                #     saturation=0.05,
+                #     hue=0.02
+                # ),
                 transforms.ToTensor(),
             ]
 
@@ -66,30 +63,30 @@ class Processor:
 
 
         if self.use_high_pass_filter:
-            transforms_list.append(
+            self.transforms_list.append(
                 transforms.Lambda(lambda x: self._high_pass_filter(x, alpha=2.0))
             )
 
         if self.use_patch_shuffle:
-            transforms_list.append(
-                transforms.Lambda(lambda x: self._patch_shuffle(x, patch_size=32))
+            self.transforms_list.append(
+                transforms.Lambda(lambda x: self._patch_shuffle_partial(x, patch_size=32, shuffle_ratio=0.4))
             )
         
         if self.use_random_erasing:
-            transforms_list.append(
+            self.transforms_list.append(
                 transforms.RandomErasing(p=0.5,
                             scale=(0.02, 0.2),
                             ratio=(0.3, 3.3))
             )
 
-        transforms_list.append(
+        self.transforms_list.append(
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406],
                 std=[0.229, 0.224, 0.225]
             )
         )
 
-        self.transform = transforms.Compose(transforms_list)
+        self.transform = transforms.Compose(self.transforms_list)
 
     def __call__(self, image):
         return self.transform(image)
@@ -140,39 +137,72 @@ class Processor:
 
         return image
     
-    def _patch_shuffle(self, image: Tensor, patch_size=32) -> Tensor:
+    def _patch_shuffle_partial(self, image: Tensor, patch_size=32, shuffle_ratio=0.6):
         C, H, W = image.shape
 
-        # nombre de patches
         n_h = H // patch_size
         n_w = W // patch_size
 
-        # reshape en grille
         image = image.view(C, n_h, patch_size, n_w, patch_size)
         image = image.permute(1, 3, 0, 2, 4)  # (n_h, n_w, C, p, p)
 
-        # flatten patches
         patches = image.reshape(-1, C, patch_size, patch_size)
 
-        # shuffle
-        idx = torch.randperm(patches.size(0))
-        patches = patches[idx]
+        num_patches = patches.size(0)
+        num_shuffle = int(shuffle_ratio * num_patches)
+
+        # indices à mélanger
+        shuffle_indices = torch.randperm(num_patches)[:num_shuffle]
+
+        # copie pour éviter overwrite
+        patches_shuffled = patches.clone()
+
+        permuted = shuffle_indices[torch.randperm(num_shuffle)]
+
+        patches_shuffled[shuffle_indices] = patches[permuted]
 
         # reconstruction
-        patches = patches.view(n_h, n_w, C, patch_size, patch_size)
-        image = patches.permute(2, 0, 3, 1, 4).contiguous()
+        patches_shuffled = patches_shuffled.view(n_h, n_w, C, patch_size, patch_size)
+        image = patches_shuffled.permute(2, 0, 3, 1, 4).contiguous()
 
         return image.view(C, H, W)
     
     def _high_pass_filter(self, image: Tensor, alpha=0.2) -> Tensor:
-        blur = transforms.GaussianBlur(kernel_size=11, sigma=(0.3, 5.0))(image)
-        image_hp = image - blur
+        # blur = transforms.GaussianBlur(kernel_size=7, sigma=(0.5, 1.0))(image)
+        # image_hp = image - blur
+        
 
-        image_hp = image_hp * alpha
+        # image_hp = image_hp * alpha
+        # image_hp = torch.abs(image_hp)
 
-        image_hp = (image_hp - image_hp.min()) / (image_hp.max() - image_hp.min() + 1e-8)
 
-        return image_hp
+        # image_hp = (image_hp - image_hp.min()) / (image_hp.max() - image_hp.min() + 1e-8)
+
+        # image_hp = image_hp ** 0.3
+
+        # return image_hp
+        blur1 = transforms.GaussianBlur(7, sigma=1.0)(image)
+        blur2 = transforms.GaussianBlur(9, sigma=2.5)(image)
+        blur_global = transforms.GaussianBlur(15, sigma=5.0)(image)
+
+        detail1 = image - blur1
+        detail2 = image - blur2
+
+        enhanced = image + 1.2 * (detail1 + 0.5 * detail2)
+
+        # 🔥 suppression structure globale
+        enhanced = enhanced - 0.3 * blur_global
+
+        # 🔥 non-linéarité texture
+        enhanced = torch.clamp(enhanced, 0.0, 1.0)
+        enhanced = enhanced ** 0.8
+
+        return enhanced
+    
+    def local_contrast_norm(self, image):
+        mean = image.mean(dim=(1,2), keepdim=True)
+        std = image.std(dim=(1,2), keepdim=True)
+        return (image - mean) / (std + 1e-8)
     
     def compute_fft_image(self, image_tensor: Tensor) -> np.ndarray:
         image_np = image_tensor.mean(dim=0).cpu().numpy()
@@ -181,61 +211,161 @@ class Processor:
         magnitude = np.log(1 + np.abs(fft_shift))
         return magnitude
 
-    def plot_processing_example(self, original_data_dir: str) -> None:
-        original_dataset = ImageFolder(
-            root=original_data_dir,
-            transform=None 
-        )
+    def plot_processing_example(self, original_data_dir: str, n_samples: int = 4):
+        original_dataset = ImageFolder(root=original_data_dir, transform=None)
 
         index = random.randint(0, len(original_dataset) - 1)
-
         image_pil, label = original_dataset[index]
 
         texture_processor = Processor(mode="texture")
-        global_processor = Processor(mode="global")
+        global_processor = Processor(mode="global", use_patch_shuffle=True)
 
-        texture_image = texture_processor(image_pil)
-        global_image = global_processor(image_pil)
+        fig, axes = plt.subplots(2, n_samples + 1, figsize=(15, 6))
 
-        texture_image = self._denormalize_image(texture_image)
-        global_image = self._denormalize_image(global_image)
+        # original
+        axes[0, 0].imshow(np.array(image_pil))
+        axes[0, 0].set_title("Original")
 
-        original_np = np.array(image_pil)
-        texture_np = texture_image.permute(1, 2, 0).cpu().numpy()
-        global_np = global_image.permute(1, 2, 0).cpu().numpy()
+        for i in range(n_samples):
+            texture_image = texture_processor(image_pil)
+            texture_image = self._denormalize_image(texture_image)
+            texture_np = texture_image.permute(1, 2, 0).cpu().numpy()
 
-        fft_texture = self.compute_fft_image(texture_image)
-        fft_global = self.compute_fft_image(global_image)
+            axes[0, i+1].imshow(texture_np)
+            axes[0, i+1].set_title(f"Texture {i}")
 
-        fig, axes = plt.subplots(2, 3, figsize=(12, 6))
+        for i in range(n_samples):
+            global_image = global_processor(image_pil)
+            global_image = self._denormalize_image(global_image)
+            global_np = global_image.permute(1, 2, 0).cpu().numpy()
 
-        axes[0,0].imshow(original_np)
-        axes[0,1].imshow(texture_np)
-        axes[0,2].imshow(global_np)
+            axes[1, i+1].imshow(global_np)
+            axes[1, i+1].set_title(f"Global {i}")
 
-        axes[1,1].imshow(fft_texture, cmap='gray')
-        axes[1,2].imshow(fft_global, cmap='gray')
+        axes[1, 0].axis("off")
 
-        axes[1,0].axis("off")
+        plt.tight_layout()
+        plt.show()
 
-        axes[0,0].set_title("Original")
-        axes[0,1].set_title("Texture")
-        axes[0,2].set_title("Global")
+    def compare_pipelines(self, image_pil):
+        texture_processor = Processor(
+            mode="texture",
+            use_patch_shuffle=True,
+            use_high_pass_filter=True
+        )
 
-        axes[1,1].set_title("FFT Texture")
-        axes[1,2].set_title("FFT Global")
+        global_processor = Processor(
+            mode="global"
+        )
+
+        def apply_pipeline(processor, image):
+            outputs = []
+            current = image
+
+            outputs.append(("Original", np.array(current)))
+
+            for transform in processor.transforms_list:
+                current = transform(current)
+
+                name = transform.__class__.__name__
+
+                if isinstance(current, torch.Tensor):
+                    img = current.clone()
+
+                    # On dénormalise seulement si on vient de passer Normalize
+                    if isinstance(transform, transforms.Normalize):
+                        img = processor._denormalize_image(img)
+
+                    img = img.permute(1, 2, 0).cpu().numpy()
+                    img = np.clip(img, 0, 1)
+                else:
+                    img = np.array(current)
+
+                outputs.append((name, img))
+
+            return outputs
+
+        texture_outputs = apply_pipeline(texture_processor, image_pil)
+        global_outputs = apply_pipeline(global_processor, image_pil)
+
+        n = max(len(texture_outputs), len(global_outputs))
+
+        fig, axes = plt.subplots(2, n, figsize=(4*n, 8))
+
+        for i in range(n):
+            if i < len(texture_outputs):
+                name, img = texture_outputs[i]
+                axes[0, i].imshow(img)
+                axes[0, i].set_title(f"Texture: {name}")
+            axes[0, i].axis("off")
+
+            if i < len(global_outputs):
+                name, img = global_outputs[i]
+                axes[1, i].imshow(img)
+                axes[1, i].set_title(f"Global: {name}")
+            axes[1, i].axis("off")
+
+        plt.tight_layout()
+        plt.show()
+
+    def plot_deterministic_transformations(self, image_pil):
+
+        transforms_to_apply = {
+            "Original": lambda x: x,
+            "Resize": transforms.Resize(224),
+            "CenterCrop": transforms.CenterCrop(224),
+            "Grayscale": transforms.Grayscale(num_output_channels=3),
+            "ColorJitter": transforms.ColorJitter(
+                brightness=0.3, contrast=0.3, saturation=0.1, hue=0.05
+            ),
+            "High Pass Filter": transforms.Compose([
+                # transforms.ColorJitter(
+                #     brightness=0.5,
+                #     contrast=0.9,
+                #     saturation=0.1,
+                #     hue=0.05
+                # ),
+                transforms.Resize(224),
+                # transforms.CenterCrop(224),
+                # transforms.Grayscale(num_output_channels=3),
+                transforms.ToTensor(),
+                # transforms.Lambda(lambda x: torch.clamp(x, 0, 1))
+                transforms.Lambda(lambda x: self._high_pass_filter(x, alpha=5.0)),
+                transforms.Lambda(lambda x: self.local_contrast_norm(x)),
+
+                ]
+                )
+        }
+
+        fig, axes = plt.subplots(1, len(transforms_to_apply), figsize=(15, 4))
+
+        for i, (name, transform) in enumerate(transforms_to_apply.items()):
+            img = transform(image_pil)
+
+            if isinstance(img, torch.Tensor):
+                img = img.permute(1, 2, 0).numpy()
+            else:
+                img = np.array(img)
+
+            axes[i].imshow(img)
+            axes[i].set_title(name)
+            axes[i].axis("off")
 
         plt.tight_layout()
         plt.show()
 
 
 
-
-
 if __name__ == "__main__":
     data_dir = "/home/rabah/data/Paysages/seg_train"
-    processor = Processor(mode="texture", use_patch_shuffle=True)
-    processor.plot_processing_example(original_data_dir=data_dir)
+    processor = Processor(mode="global", use_patch_shuffle=True)
+    # processor.plot_processing_example(original_data_dir=data_dir)
+    dataset = ImageFolder(data_dir, transform=None)
+    image, _ = dataset[0]
+
+    processor.plot_deterministic_transformations(image)
+
+    # processor.compare_pipelines(image)
     # processor = Processor(mode="texture")
     # texture_dataset, global_dataset = Processor.create_datasets(data_dir=data_dir)
     # processor.plot_processing_example(original_data_dir=data_dir, texture_dataset=texture_dataset, global_dataset=global_dataset)
